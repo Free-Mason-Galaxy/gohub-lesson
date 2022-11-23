36ac1c4f2ab7e2bdfe95255a3fb08912125a6aff
rk <kai.ran@foxmail.com>
2022-11-23 17:05:26 +0800

    数据填充

---
+++ database/seeders/seeder.go
@@ -0,0 +1,20 @@
+// Package seeders
+// descr
+// author fm
+// date 2022/11/23 17:04
+package seeders
+
+import (
+	"gohub-lesson/database/seed"
+)
+
+// Initialize 初始化
+func Initialize() {
+
+	// 触发加载本目录下其他文件中的 init 方法
+
+	// 指定优先于同目录下的其他文件运行
+	seed.SetRunOrder([]string{
+		"SeedUsersTable",
+	})
+}
---
+++ database/seeders/users_seeder.go
@@ -0,0 +1,36 @@
+// Package seeders
+// descr
+// author fm
+// date 2022/11/23 17:03
+package seeders
+
+import (
+	"fmt"
+
+	"gohub-lesson/database/factories"
+	"gohub-lesson/database/seed"
+	"gohub-lesson/pkg/console"
+	"gohub-lesson/pkg/logger"
+	"gorm.io/gorm"
+)
+
+func init() {
+	// 添加 Seeder
+	seed.Add("SeedUsersTable", func(db *gorm.DB) {
+
+		// 创建 10 个用户对象
+		users := factories.MakeUsers(10)
+
+		// 批量创建用户（注意批量创建不会调用模型钩子）
+		result := db.Table("users").Create(&users)
+
+		// 记录错误
+		if err := result.Error; err != nil {
+			logger.LogIf(err)
+			return
+		}
+
+		// 打印运行情况
+		console.Success(fmt.Sprintf("Table [%v] %v rows seeded", result.Statement.Table, result.RowsAffected))
+	})
+}
