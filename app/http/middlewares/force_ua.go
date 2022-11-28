066129b987889dc957dd3ebc2e3f6247a9f3babd
rk <kai.ran@foxmail.com>
2022-11-28 16:34:48 +0800

    API 域名和前缀

--- app/http/middlewares/force_ua.go
+++ app/http/middlewares/force_ua.go
@@ -18,7 +18,9 @@ func ForceUA() gin.HandlerFunc {
 
 		// 获取 User-Agent 标头信息
 		if len(c.GetHeader("User-Agent")) == 0 {
-			response.BadRequest(c, errors.New("User-Agent 标头未找到"), "请求必须附带 User-Agent 标头")
+			response.BadRequest(c,
+				errors.New("User-Agent 标头未找到"),
+				"请求必须附带 User-Agent 标头")
 			return
 		}
 
